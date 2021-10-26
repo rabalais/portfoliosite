edfe26af07f49a86e77301027d630042aa277387
Noah Rabalais <60150831+rabalais@users.noreply.github.com>
2021-10-26 07:00:33 -0500

    commit

--- main.js
+++ main.js
@@ -2,4 +2,5 @@ $(document).ready(function () {
     particlesJS.load("background", "js-particles/particlesjs-config.json", function(){
         console.log("callback - particles.js config loaded | Thanks Vincent Garreau :)")
     });
+
 });
