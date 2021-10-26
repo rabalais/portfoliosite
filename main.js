c23ad5216522dfce36d03d4ab05aa8b2657f11cc
Noah Rabalais <60150831+rabalais@users.noreply.github.com>
2021-10-26 02:01:50 -0500

    overhaul

--- main.js
+++ main.js
@@ -1,25 +1,5 @@
 $(document).ready(function () {
-    navHandler();
     particlesJS.load("background", "js-particles/particlesjs-config.json", function(){
         console.log("callback - particles.js config loaded | Thanks Vincent Garreau :)")
     });
-
-    $("#left").click(function () {
-
-    });
-    $("#right").click(function () {
-
-    });
 });
-
-function navHandler() {
-    var navContainer = document.getElementById("nav");
-    var btns = navContainer.getElementsByClassName("btn");
-    for (var i = 0; i < btns.length; i++) {
-        btns[i].addEventListener("click", function () {
-            var current = document.getElementsByClassName("active");
-            current[0].className = current[0].className.replace(" active", "");
-            this.className += " active";
-        });
-    }
-}
